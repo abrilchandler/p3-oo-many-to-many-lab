38945d013d5092bede9e3c79b90a2f98edff32f3
Chandler Abril <abrilchandler1@gmail.com>
2024-07-31 20:38:33 -0700

    Almost done

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -10,30 +10,36 @@ class Book:
     def get_all_books(self):
         return self.all_books
     
+    def contracts(self):
+        return [contract for contract in Contract.get_all_contracts() if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 class Author:
     all_authors = []
 
-    def __init__(self, name, author_contracts=[]):
+    def __init__(self, name, author_contracts=None):
         self.name = name
         self.all_authors.append(self)
-        self.author_contracts = author_contracts
-    
-    
+        self.author_contracts = author_contracts or []
+
     def contracts(self):
         return self.author_contracts
     
-    #incorrect/not working
-    def books(self):
-        related_books = []
-        for contract in self.author_contracts:
-            related_books.append(contract.book)
-        return related_books
-
     def add_contracts(self, contract):
         self.author_contracts.append(contract)
 
+    def books(self):
+        return [contract.book for contract in self.author_contracts]
 
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        self.add_contracts(contract)
+        return contract
+    
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.author_contracts)
 
 class Contract:
 
@@ -56,9 +62,10 @@ class Contract:
         self.royalties = royalties
         self.all_contracts.append(self)
 
+        author.add_contracts(self)
+
     @classmethod
     def get_all_contracts(self):
         return self.all_contracts
     
-    
-     
+   
